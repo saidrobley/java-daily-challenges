4a246a92658930ba86bc45e33931a14bbe3a41d5
Said Robley <saidrobley@gmail.com>
2019-05-13 14:17:11 -0600

    create simpler implementations

--- Day97/SubsetsTwo.java
+++ Day97/SubsetsTwo.java
@@ -22,18 +22,25 @@ public class SubsetsTwo {
   public List<List<Integer>> subsetsWithDup(int[] nums) {
     Arrays.sort(nums);
     List<List<Integer>> result = new ArrayList<>();
-    sub(result, new ArrayList<>(nums.length), nums, 0);
+    List<Integer> subset = new ArrayList<>();
+    if (nums == null || nums.length == 0) {
+      return result;
+    }
+
+    findAllSubset(nums, result, subset, 0);
     return result;
   }
 
-  private static void sub(List<List<Integer>> result, List<Integer> cur, int[] nums, int pos) {
-    result.add(new ArrayList<>(cur));
-    for (int i = pos; i < nums.length; i++) {
-      if (i == pos || nums[i] != nums[i - 1]) {
-        cur.add(nums[i]);
-        sub(result, cur, nums, i + 1);
-        cur.remove(cur.size() - 1);
+  private void findAllSubset(int[] nums, List<List<Integer>> result, List<Integer> subset, int position) {
+
+    result.add(new ArrayList<>(subset));
+    for (int i = position; i < nums.length; i++) {
+      if (i > position && nums[i] == nums[i - 1]) {
+        continue;
       }
+      subset.add(nums[i]);
+      findAllSubset(nums, result, subset, i + 1);
+      subset.remove(subset.size() - 1);
     }
   }
 }
